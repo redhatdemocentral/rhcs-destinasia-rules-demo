d87cd22d497a45fbbe2a63a83526ddba988c4ee0
Eric D. Schabell <eric@schabell.org>
2017-02-17 12:55:50 +0100

    Initial import Destinasia travel discount rules in JBoss BRMS on OCP.

---
+++ init.bat
@@ -0,0 +1,213 @@
+@ECHO OFF
+setlocal enableextensions enabledelayedexpansion
+
+set PROJECT_HOME=%~dp0
+set DEMO=APAC Destinasia Travel Rules Demo
+set AUTHORS=Andrew Block, Eric D. Schabell
+set PROJECT=git@github.com:redhatdemocentral/apac-destinasia-rules-demo.git
+set SRC_DIR=%PROJECT_HOME%installs
+set OPENSHIFT_USER=openshift-dev
+set OPENSHIFT_PWD=devel
+set HOST_IP=10.1.2.2
+set BRMS=jboss-brms-6.4.0.GA-deployable-eap7.x.zip
+set EAP=jboss-eap-7.0.0-installer.jar
+
+REM wipe screen.
+cls
+
+echo.
+echo ###################################################################
+echo ##                                                               ##   
+echo ##  Setting up the %DEMO%                 ##
+echo ##                                                               ##
+echo ##                                                               ##   
+echo ##  ####  ####  #   #  ####        #### #      ###  #   # ####   ##
+echo ##  #   # #   # ## ## #       #   #     #     #   # #   # #   #  ##
+echo ##  ####  ####  # # #  ###   ###  #     #     #   # #   # #   #  ##
+echo ##  #   # # #   #   #     #   #   #     #     #   # #   # #   #  ##
+echo ##  ####  #  #  #   # ####         #### #####  ###   ###  ####   ##
+echo ##                                                               ##   
+echo ##  brought to you by,                                           ##   
+echo ##             %AUTHORS%                    ##
+echo ##                                                               ##
+echo ##  %PROJECT%  ##
+echo ##                                                               ##   
+echo ###################################################################
+echo.
+
+REM Validate OpenShift 
+set argTotal=0
+
+for %%i in (%*) do set /A argTotal+=1
+
+if %argTotal% EQU 1 (
+
+    call :validateIP %1 valid_ip
+
+	if !valid_ip! EQU 0 (
+	    echo OpenShift host given is a valid IP...
+	    set HOST_IP=%1
+		echo.
+		echo Proceeding with OpenShift host: !HOST_IP!...
+	) else (
+		echo Please provide a valid IP that points to an OpenShift installation...
+		echo.
+        GOTO :printDocs
+	)
+
+)
+
+if %argTotal% GTR 1 (
+    GOTO :printDocs
+)
+
+
+REM make some checks first before proceeding.	
+call where oc >nul 2>&1
+if  %ERRORLEVEL% NEQ 0 (
+	echo OpenShift command line tooling is required but not installed yet... download here:
+	echo https://access.redhat.com/downloads/content/290
+	GOTO :EOF
+)
+
+if exist "%SRC_DIR%\%EAP%" (
+        echo Product EAP sources are present...
+        echo.
+) else (
+        echo Need to download %EAP% package from developers.redhat.com
+        echo and place it in the %SRC_DIR% directory to proceed...
+        echo.
+        GOTO :EOF
+)
+
+if exist "%SRC_DIR%\%BRMS%" (
+        echo Product BPM Suite sources are present...
+        echo.
+) else (
+        echo Need to download %BRMS% package from http://developers.redhat.com
+        echo and place it in the %SRC_DIR% directory to proceed...
+        echo.
+        GOTO :EOF
+)
+
+echo OpenShift commandline tooling is installed...
+echo.
+echo Logging in to OpenShift as %OPENSHIFT_USER%...
+echo.
+call oc login %HOST_IP%:8443 --password="%OPENSHIFT_PWD%" --username="%OPENSHIFT_USER%"
+
+if not "%ERRORLEVEL%" == "0" (
+  echo.
+	echo Error occurred during 'oc login' command!
+	echo.
+	GOTO :EOF
+)
+
+echo.
+echo Creating a new project...
+echo.
+call oc new-project apac-destinasia-travel
+
+echo.
+echo Setting up a new build...
+echo.
+call oc new-build "jbossdemocentral/developer" --name=destinasia-rules-demo --binary=true
+
+if not "%ERRORLEVEL%" == "0" (
+  echo.
+	echo Error occurred during 'oc new-build' command!
+	echo.
+	GOTO :EOF
+)
+
+REM need to wait a bit for new build to finish with developer image.
+timeout 10 /nobreak
+
+echo.
+echo Importing developer image...
+echo.
+call oc import-image developer
+
+if not "%ERRORLEVEL%" == "0" (
+  echo.
+	echo Error occurred during 'oc import-image' command!
+	echo.
+	GOTO :EOF
+)
+
+echo.
+echo Starting a build, this takes some time to upload all of the product sources for build...
+echo.
+call oc start-build destinasia-rules-demo --from-dir=. --follow=true  --wait=true
+
+if not "%ERRORLEVEL%" == "0" (
+  echo.
+	echo Error occurred during 'oc start-build' command!
+	echo.
+	GOTO :EOF
+)
+
+echo.
+echo Creating a new application...
+echo.
+call oc new-app destinasia-rules-demo
+
+if not "%ERRORLEVEL%" == "0" (
+  echo.
+	echo Error occurred during 'oc new-app' command!
+	echo.
+	GOTO :EOF
+)
+
+echo.
+echo Creating an externally facing route by exposing a service...
+echo.
+call oc expose service destinasia-rules-demo --hostname=destinasia-rules-demo.%HOST_IP%.xip.io
+
+if not "%ERRORLEVEL%" == "0" (
+  echo.
+	echo Error occurred during 'oc expose service' command!
+	echo.
+	GOTO :EOF
+)
+
+echo.
+echo ====================================================================
+echo =                                                                  =
+echo =  Login to JBoss BRMS to start developing rules projects:         =
+echo =                                                                  =
+echo =  http://destinasia-rules-demo.%HOST_IP%.xip.io/business-central =
+echo =                                                                  =
+echo =  [ u:erics / p:jbossbrms1! ]                                     =
+echo =                                                                  =
+echo ====================================================================
+echo.
+
+GOTO :EOF
+      
+
+:validateIP ipAddress [returnVariable]
+
+    setlocal 
+
+    set "_return=1"
+
+    echo %~1^| findstr /b /e /r "[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*" >nul
+
+    if not errorlevel 1 for /f "tokens=1-4 delims=." %%a in ("%~1") do (
+        if %%a gtr 0 if %%a lss 255 if %%b leq 255 if %%c leq 255 if %%d gtr 0 if %%d leq 254 set "_return=0"
+    )
+
+:endValidateIP
+
+    endlocal & ( if not "%~2"=="" set "%~2=%_return%" ) & exit /b %_return%
+	
+:printDocs
+  echo This project can be installed on any OpenShift platform. It's possible to
+  echo install it on any available installation by pointing this installer to an
+  echo OpenShift IP address:
+  echo.
+  echo   $ ./init.sh IP
+  echo.
+  echo If using Red Hat OCP, IP should look like: 192.168.99.100
+  echo.
---
+++ init.sh
@@ -0,0 +1,209 @@
+#!/bin/sh 
+DEMO="APAC Destinasia Travel Rules Demo"
+AUTHORS="Andrew Block, Eric D. Schabell"
+PROJECT="git@github.com:redhatdemocentral/apac-destinasia-rules-demo.git"
+SRC_DIR=./installs
+OPENSHIFT_USER=openshift-dev
+OPENSHIFT_PWD=devel
+HOST_IP=10.1.2.2
+BRMS=jboss-brms-6.4.0.GA-deployable-eap7.x.zip
+EAP=jboss-eap-7.0.0-installer.jar
+
+# prints the documentation for this script.
+function print_docs() 
+{
+	echo "This project can be installed on any OpenShift platform, such as OpenShift"
+	echo "Container Platform or Red Hat Container Development Kit. It's possible to"
+	echo "install it on any available installation by pointing this installer to an"
+	echo "OpenShift IP address:"
+	echo
+	echo "   $ ./init.sh IP"
+	echo
+	echo "If using Red Hat OCP, IP should look like: 192.168.99.100"
+	echo
+}
+
+# check for a valid passed IP address.
+function valid_ip()
+{
+	local  ip=$1
+	local  stat=1
+
+	if [[ $ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
+		OIFS=$IFS
+		IFS='.'
+		ip=($ip)
+		IFS=$OIFS
+		[[ ${ip[0]} -le 255 && ${ip[1]} -le 255 && ${ip[2]} -le 255 && ${ip[3]} -le 255 ]]
+		stat=$?
+	fi
+
+	return $stat
+}
+
+# wipe screen.
+clear 
+
+echo
+echo "#######################################################################"
+echo "##                                                                   ##"   
+echo "##  Setting up the ${DEMO}                 ##"
+echo "##                                                                   ##"   
+echo "##                                                                   ##"   
+echo "##  ####  ####  #   #  ####        #### #      ###  #   # ####       ##"
+echo "##  #   # #   # ## ## #       #   #     #     #   # #   # #   #      ##"
+echo "##  ####  ####  # # #  ###   ###  #     #     #   # #   # #   #      ##"
+echo "##  #   # # #   #   #     #   #   #     #     #   # #   # #   #      ##"
+echo "##  ####  #  #  #   # ####         #### #####  ###   ###  ####       ##"
+echo "##                                                                   ##"   
+echo "##  brought to you by,                                               ##"   
+echo "##             ${AUTHORS}                        ##"
+echo "##                                                                   ##"   
+echo "##  ${PROJECT}  ##"
+echo "##                                                                   ##"   
+echo "#######################################################################"
+echo
+
+# validate OpenShift host IP.
+if [ $# -eq 1 ]; then
+	if valid_ip $1; then
+		echo "OpenShift host given is a valid IP..."
+		HOST_IP=$1
+		echo
+		echo "Proceeding wiht OpenShift host: $HOST_IP..."
+		echo
+	else
+		# bad argument passed.
+		echo "Please provide a valid IP that points to an OpenShift installation..."
+		echo
+		print_docs
+		echo
+		exit
+	fi
+elif [ $# -gt 1 ]; then
+	print_docs
+	echo
+	exit
+else
+	# no arguments, prodeed with default host.
+	print_docs
+	echo
+	exit
+fi
+
+# make some checks first before proceeding.	
+command -v oc -v >/dev/null 2>&1 || { echo >&2 "OpenShift command line tooling is required but not installed yet... download here: https://access.redhat.com/downloads/content/290"; exit 1; }
+
+# make some checks first before proceeding.	
+if [ -r $SRC_DIR/$EAP ] || [ -L $SRC_DIR/$EAP ]; then
+	echo Product EAP sources are present...
+	echo
+else
+	echo Need to download $EAP package from http://developers.redhat.com
+	echo and place it in the $SRC_DIR directory to proceed...
+	echo
+	exit
+fi
+
+if [ -r $SRC_DIR/$BRMS ] || [ -L $SRC_DIR/$BRMS ]; then
+	echo Product BRMS sources are present...
+	echo
+else
+	echo Need to download $BRMS from http://developers.redhat.com
+	echo and place it in the $SRC_DIR directory to proceed...
+	echo
+	exit
+fi
+
+echo "OpenShift commandline tooling is installed..."
+echo 
+echo "Logging in to OpenShift as $OPENSHIFT_USER..."
+echo
+oc login $HOST_IP:8443 --password=$OPENSHIFT_PWD --username=$OPENSHIFT_USER
+
+
+if [ $? -ne 0 ]; then
+	echo
+	echo Error occurred during 'oc login' command!
+	exit
+fi
+
+echo
+echo "Creating a new project..."
+echo
+oc new-project apac-destinasia-travel
+
+echo
+echo "Setting up a new build..."
+echo
+oc new-build "jbossdemocentral/developer" --name=destinasia-rules-demo --binary=true
+
+if [ $? -ne 0 ]; then
+	echo
+	echo Error occurred during 'oc new-build' command!
+	exit
+fi
+
+# need to wait a bit for new build to finish with developer image.
+sleep 10   
+
+echo
+echo "Importing developer image..."
+echo
+oc import-image developer
+
+if [ $? -ne 0 ]; then
+	echo
+	echo Error occurred during 'oc import-image' command!
+	exit
+fi
+
+echo
+echo "Starting a build, this takes some time to upload all of the product sources for build..."
+echo
+oc start-build destinasia-rules-demo --from-dir=. --follow=true --wait=true
+
+if [ $? -ne 0 ]; then
+	echo
+	echo Error occurred during 'oc start-build' command!
+	exit
+fi
+
+# need to wait a bit for new build to finish with developer image.
+sleep 3
+
+echo
+echo "Creating a new application..."
+echo
+oc new-app destinasia-rules-demo
+
+if [ $? -ne 0 ]; then
+	echo
+	echo Error occurred during 'oc new-app' command!
+	exit
+fi
+
+echo
+echo "Creating an externally facing route by exposing a service..."
+echo
+oc expose service destinasia-rules-demo --hostname="destinasia-rules-demo.$HOST_IP.xip.io"
+
+if [ $? -ne 0 ]; then
+	echo
+	echo Error occurred during 'oc expose service' command!
+	exit
+fi
+
+echo
+echo "==========================================================================="
+echo "=                                                                         ="
+echo "=  Login to JBoss BRMS to start developing rules projects:                ="
+echo "=                                                                         ="
+echo "=  http://destinasia-rules-demo.$HOST_IP.xip.io/business-central    ="
+echo "=                                                                         ="
+echo "=  [ u:erics / p:jbossbrms1! ]                                            ="
+echo "=                                                                         ="
+echo "=  Note: it takes a few minutes to expose the service...                  ="
+echo "=                                                                         ="
+echo "==========================================================================="
+
