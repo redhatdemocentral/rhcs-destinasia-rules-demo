c09c1680582e5ec0a2944d5d481256769ce19f6b
Eric D. Schabell <eric@schabell.org>
2017-03-02 23:50:27 +0800

    Added proper export commands for tools in both init scripts.

--- init.sh
+++ init.sh
@@ -10,9 +10,9 @@ BRMS=jboss-brms-6.4.0.GA-deployable-eap7.x.zip
 EAP=jboss-eap-7.0.0-installer.jar
 
 # set ansible environment variables.
-export OC_LOC=`which oc`
-export SED_LOC=`which sed`
-export AWK_LOC=`which awk`
+export OC_LOC=$(which oc)
+export SED_LOC=$(which sed)
+export AWK_LOC=$(which awk)
 
 # prints the documentation for this script.
 function print_docs() 
